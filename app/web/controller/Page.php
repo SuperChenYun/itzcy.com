5728a2cd97f507cb78dc591257745498e1f10895
itzcy <itzcy@itzcy.com>
2021-02-11 10:36:40 +0800

    Page功能展示

--- app/web/controller/Page.php
+++ app/web/controller/Page.php
@@ -4,14 +4,36 @@
 namespace app\web\controller;
 
 
+use app\service\LinkService;
+use app\service\MenuService;
+use app\service\PageService;
+use app\service\SettingService;
 use app\web\BaseController;
+use think\App;
 use think\facade\View;
 
+/**
+ * @property PageService pageService
+ */
 class Page extends BaseController
 {
+    public function __construct (App $app, MenuService $menuService, LinkService $linkService, SettingService $settingService, PageService $pageService)
+    {
+        parent ::__construct($app, $menuService, $linkService, $settingService);
+        
+        $this->pageService = $pageService;
+        
+    }
     
     public function read(Int $id)
     {
+        
+        $page = $this->pageService->read($id);
+        if (empty($page)) {
+            return View::fetch('error/404');
+        }
+        View::assign('page', $page);
+        return View::fetch('page/page');
     
     }
     
